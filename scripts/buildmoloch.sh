bf1fa82c770c4c1ca439d19743696e878283cf49
Vlatko Kosturjak <kost@linux.hr>
2015-06-10 10:44:02 +0200

    FIx potential conflict in USERNAME var

--- scripts/buildmoloch.sh
+++ scripts/buildmoloch.sh
@@ -12,7 +12,7 @@ echo "git clone"
 git clone -b batch-build-fix https://github.com/kost/moloch.git $BUILDDIR
 echo "cd to dir and build"
 cd $BUILDDIR
-USEPFRING=no ESMEM="512M" DONOTSTART=yes USERNAME=daemon GROUPNAME=daemon PASSWORD=0mgMolochDockerRules5 INTERFACE=eth0 BATCHRUN=yes ./easybutton-singlehost.sh
+USEPFRING=no ESMEM="512M" DONOTSTART=yes MOLOCHUSER=daemon GROUPNAME=daemon PASSWORD=0mgMolochDockerRules5 INTERFACE=eth0 BATCHRUN=yes ./easybutton-singlehost.sh
 killall java
 echo "Giving ES time to shut itself"
 sleep 5
