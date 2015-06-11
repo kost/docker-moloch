f10cd3e035638e2cdbdcb3383eacbab751bfd53a
Vlatko Kosturjak <kost@linux.hr>
2015-06-11 08:42:00 +0200

    Use original repository once pull is accepted

--- scripts/buildmoloch.sh
+++ scripts/buildmoloch.sh
@@ -9,7 +9,7 @@ else
 fi
 
 echo "git clone"
-git clone -b batch-build-fix https://github.com/kost/moloch.git $BUILDDIR
+git clone https://github.com/aol/moloch.git $BUILDDIR
 echo "cd to dir and build"
 cd $BUILDDIR
 USEPFRING=no ESMEM="512M" DONOTSTART=yes MOLOCHUSER=daemon GROUPNAME=daemon PASSWORD=0mgMolochDockerRules5 INTERFACE=eth0 BATCHRUN=yes ./easybutton-singlehost.sh
